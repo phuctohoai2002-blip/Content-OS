fb076041e3739fde0a0e2f2e034aed96d45a51bc
phuctohoai2002-blip <phuctohoai.2002@gmail.com>
2026-08-22 21:27:31 +0700

    feat: initialize taxonomy and popup enhancements

---
+++ js/ui-enhancements.js
@@ -0,0 +1,6 @@
+import { initTaxonomyAddAll } from "./taxonomy-add-all.js";
+import "./add-popup-bulk-link.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initTaxonomyAddAll();
+});
