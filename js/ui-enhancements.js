bfa0669a886c76fd06ac20e908458da38c5fe43d
phuctohoai2002-blip <phuctohoai.2002@gmail.com>
2026-09-03 23:58:53 +0700

    Simplify UI enhancement loader

--- js/ui-enhancements.js
+++ js/ui-enhancements.js
@@ -1,6 +1,1 @@
-import { initTaxonomyAddAll } from "./taxonomy-add-all.js";
 import "./add-popup-bulk-link.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initTaxonomyAddAll();
-});
