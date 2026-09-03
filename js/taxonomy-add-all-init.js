9432396c8fcb28b6153c5b7c21278e3940ef87d1
phuctohoai2002-blip <phuctohoai.2002@gmail.com>
2026-09-03 23:58:28 +0700

    Remove obsolete taxonomy loader

--- js/taxonomy-add-all-init.js
+++
@@ -1,4 +0,0 @@
-import { initTaxonomyAddAllV2 } from "./taxonomy-add-all-v2.js";
-
-const start = () => initTaxonomyAddAllV2();
-if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", start, { once: true }); else start();
